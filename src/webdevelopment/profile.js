2311f6a1522705183b592d8fe792e0c3fcd7980f
Cress2519 <cressbabor17@gmail.com>
2025-06-20 01:43:37 +0000

    activity six

---
+++ src/webdevelopment/profile.js
@@ -0,0 +1,9 @@
+export default function Act1(){
+    return(
+        <div>
+            <h1>MY PROFILE</h1>
+            <p>Name: Wenna Marie Angela Delfin</p>
+            <p>Section: D</p>
+        </div>
+    );
+}
